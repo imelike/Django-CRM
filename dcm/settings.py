68a5480d0fb3109fa22de21a40bbad9ebbef8121
imelike <usermelike@gmail.com>
2024-03-18 13:39:05 +0300

    docker-update

--- dcm/settings.py
+++ dcm/settings.py
@@ -1,3 +1,4 @@
+
 from pathlib import Path
 import os
 from decouple import config
@@ -13,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool, default=True)
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
